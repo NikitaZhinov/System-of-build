7acec77e58f703857434004010a0d753cd091131
d1orn <zhinovnikita@yandex.ru>
2024-07-01 14:23:51 +0700

    qq

--- include/common.h
+++ include/common.h
@@ -1,16 +1,7 @@
 #pragma once
 
-#include <fstream>
-#include <print>
-#include <string>
-#include <vector>
-
 enum Error {
     OK,
     File_not_found,
     Invalid_input_parametrs,
-};
-
-struct Variable {
-    std::string name;
 };
--- include/parser.h
+++ include/parser.h
@@ -2,7 +2,15 @@
 
 #include "common.h"
 
+#include <fstream>
+#include <vector>
+
 class Parser {
+private:
+    static const std::vector<char> OPERATORS;
+
+    static bool findOperator(char op);
+
 public:
     static Error parsFile(std::ifstream &file);
 };
